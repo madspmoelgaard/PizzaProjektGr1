399fee29dca7b7d7ae485f05749e224c758f6223
MadsMoelgaard <mads@MacBook-Pro.local>
2025-03-20 14:52:36 +0100

    TEST DEV PUSH

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
@@ -58,6 +60,7 @@ public class Main {
                 //tilføj til ordrehistorik her
             }
         }
+        System.out.println("FARVEL");
     }
 }
 
