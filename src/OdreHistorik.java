1ed7fa8210c03aef274ae8fbf21a51afd45d7114
MadsMoelgaard <mads@MacBook-Pro.local>
2025-03-20 15:14:00 +0100

    test kommentar

--- src/OdreHistorik.java
+++ src/OdreHistorik.java
@@ -2,6 +2,6 @@ public class OdreHistorik {
     //ArrayList til at gemme bestillinger til at lave omsætning og statistik
     //Blok til at vise omsætning med antal pizza, og samlet pris
     //"Get number of orders"
-
+//TEST KOMMENTAR
 
 }
